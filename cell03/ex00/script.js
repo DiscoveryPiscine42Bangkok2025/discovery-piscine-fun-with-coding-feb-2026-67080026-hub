1566f20ae0736117763714c4b12f9b30dd08dda3
67080026-hub <67080026@kmitl.ac.th>
2026-02-03 20:37:53 +0700

    Add files via upload

---
+++ cell03/ex00/script.js
@@ -0,0 +1,16 @@
+
+const btn = document.getElementById('colorButton');
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+btn.addEventListener('click', function() {
+    const newColor = getRandomColor();
+    document.body.style.backgroundColor = newColor;
+});
